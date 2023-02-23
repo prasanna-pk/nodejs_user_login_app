fe5cc4b04cbbc6a07d4d2914099280cae361ce86
Prasanna Kumar <prasannakumar.b.28.1999@gmail.com>
2023-02-23 13:17:52 +0530

    Unit testing changes done

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -16,6 +16,7 @@ exports.create = async (req, res) => {
             success: false,
             message: "Missing required fields"
         });
+        return;
     }
 
     const password = req.body.Password;
@@ -32,10 +33,20 @@ exports.create = async (req, res) => {
 
     User.create(user_data, (err, data) => {
         if (err) {
-            res.status(500).send({
-                success: false,
-                message: err.message || "error"
-            })
+            if (err.message.includes('Duplicate')) {
+                res.status(400).send({
+                    success: false,
+                    message: "User with email id " + user_data.email + " already exists."
+                })
+                return;
+            }
+            else {
+                res.status(500).send({
+                    success: false,
+                    message: err.message || "error"
+                })
+                return;
+            }
         }
         else res.send(data);
     });
@@ -47,6 +58,7 @@ exports.login = async (req, res) => {
             success: false,
             message: "Missing required fields"
         });
+        return;
     }
 
     var email_id = req.body.email;
@@ -59,17 +71,19 @@ exports.login = async (req, res) => {
                     success: false,
                     message: `User not found with email ${email_id}.`
                 });
+                return;
             } else {
                 res.status(500).send({
                     success: false,
                     message: "Error retrieving user with email " + email_id
                 });
+                return;
             }
         } else {
             const comparison = await bcrypt.compare(password, data.Password);
             if (comparison) {
                 let token = jwt.sign({
-                    exp: Math.floor(Date.now() / 1000) + (60 * 60),
+                    exp: Math.floor(Date.now() / 1000) + (60 * 60), //token with 1 hour expiry
                     data: email_id,
                 }, 'secret');
 
@@ -77,12 +91,14 @@ exports.login = async (req, res) => {
                     success: true,
                     Token: token
                 });
+                return;
             }
             else {
                 res.status(500).send({
                     success: false,
                     message: "Incorrect email id or password"
                 });
+                return;
             }
         }
     })
@@ -95,6 +111,7 @@ exports.get_users = (req, res) => {
             success: false,
             message: "Missing the required query parameter: name"
         });
+        return;
     }
 
     var name = req.query.name;
@@ -106,28 +123,32 @@ exports.get_users = (req, res) => {
                     success: false,
                     message: `No users found with name ${name}.`
                 });
+                return;
             } else {
                 res.status(500).send({
                     success: false,
                     message: "Error retrieving users with name: " + name
                 });
+                return;
             }
         } else {
             res.send({
                 success: true,
                 users: data
             });
+            return;
         }
     });
 }
 
 exports.update_user = (req, res) => {
 
-    if (!req.body || !req.body.hasOwnProperty('email') || !req.body.hasOwnProperty('name') || !req.body.hasOwnProperty('Phone_number')) {
+    if (!req.body || (!req.body.hasOwnProperty('email') && !req.body.hasOwnProperty('name') && !req.body.hasOwnProperty('Phone_number'))) {
         res.status(400).send({
             success: false,
             message: "Missing required fields"
         });
+        return;
     }
 
     if (!req.params || !req.params.hasOwnProperty('id')) {
@@ -135,15 +156,14 @@ exports.update_user = (req, res) => {
             success: false,
             message: "Missing user id path parameter"
         });
+        return;
     }
 
+    var user_data = req.body;
+    user_data.USER_UPDATED_DATE = new Date();
+
     var payload = {
-        userdata: {
-            email: req.body.email,
-            name: req.body.name,
-            Phone_number: req.body.Phone_number,
-            USER_UPDATED_DATE: new Date()
-        },
+        userdata: user_data,
         user_id: req.params.id
     };
 
@@ -153,11 +173,13 @@ exports.update_user = (req, res) => {
                 success: false,
                 message: "User update failed"
             });
+            return;
         } else {
             res.send({
                 success: true,
                 user: payload.userdata
             });
+            return;
         }
     });
 }   
